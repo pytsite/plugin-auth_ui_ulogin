a416bb4e66c5e3dc89f1c702a983a9caddeca0ae
Alexander Shepetko <a@shepetko.com>
2017-12-13 15:39:01 +0200

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -6,21 +6,27 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
-    from pytsite import tpl, lang
-    from plugins import assetman, auth, auth_ui
-    from . import _driver
+def plugin_load():
+    from pytsite import lang
+    from plugins import assetman
 
     lang.register_package(__name__)
-    tpl.register_package(__name__)
+    assetman.register_package(__name__)
 
+
+def plugin_load_console():
+    from plugins import assetman
+
+    assetman.js_module('auth-ulogin-widget', __name__ + '@widget')
+    assetman.t_js(__name__)
+    assetman.t_less(__name__)
+
+
+def plugin_load_uwsgi():
+    from pytsite import tpl
+    from plugins import auth, auth_ui
+    from . import _driver
+
+    tpl.register_package(__name__)
     auth.register_auth_driver(_driver.Auth())
     auth_ui.register_driver(_driver.UI())
-
-    assetman.register_package(__name__)
-    assetman.js_module('auth-ulogin-widget', __name__ + '@widget')
-    assetman.t_js(__name__ + '@**')
-    assetman.t_less(__name__ + '@**')
-
-
-_init()
