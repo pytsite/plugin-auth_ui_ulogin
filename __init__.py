022b598c709c993e450f42b904dd9d7962d0a709
Alexander Shepetko <a@shepetko.com>
2017-12-21 11:27:49 +0200

    Minor update 1.1.4

--- __init__.py
+++ __init__.py
@@ -6,16 +6,29 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def plugin_load():
-    from pytsite import lang
+def _register_assetman_resources():
     from plugins import assetman
 
-    lang.register_package(__name__)
-    assetman.register_package(__name__)
+    if not assetman.is_package_registered(__name__):
+        assetman.register_package(__name__)
+        assetman.js_module('auth-ulogin-widget', __name__ + '@widget')
+        assetman.t_js(__name__)
+        assetman.t_less(__name__)
 
-    assetman.js_module('auth-ulogin-widget', __name__ + '@widget')
-    assetman.t_js(__name__)
-    assetman.t_less(__name__)
+    return assetman
+
+
+def plugin_install():
+    assetman = _register_assetman_resources()
+    assetman.build(__name__)
+    assetman.build_translations()
+
+
+def plugin_load():
+    from pytsite import lang
+
+    _register_assetman_resources()
+    lang.register_package(__name__)
 
 
 def plugin_load_uwsgi():
@@ -26,11 +39,3 @@ def plugin_load_uwsgi():
     tpl.register_package(__name__)
     auth.register_auth_driver(_driver.Auth())
     auth_ui.register_driver(_driver.UI())
-
-
-def plugin_install():
-    from plugins import assetman
-
-    plugin_load()
-    assetman.build(__name__)
-    assetman.build_translations()
